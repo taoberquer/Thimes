bf475e73840847bad9e9153c44ccdb6aa6f5a71f
Tao BERQUER <tao.berquer@gmail.com>
2018-10-28 23:00:02 +0100

    Structure du projet.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,30 @@
 <?php
 
-require __DIR__  . '/../vendor/autoload.php';
+require dirname(__DIR__)  . '/vendor/autoload.php';
+
+require (dirname(__DIR__) . '/config.php');
+
+try
+{
+    $bdd = new PDO ("mysql:host=$host;dbname=$dbname",$user,$password);
+}
+catch(Exception $e)
+{
+    die("bdd non trouvée");
+}
+
+$page = "accueil";
+
+if(isset($_GET['url']))
+{
+    $page = $_GET['url'];
+
+    if(!file_exists(__DIR__ . "/pages/" . $_GET['url'] . ".php"))
+    {
+        $page = "notFound";
+    }
+}
+
+$content =  __DIR__ . '/pages/' . $page . '.php';
+
+require "layout.php";
