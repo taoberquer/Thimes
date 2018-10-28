d1ef22ff65e0bca8cab73b0a5372bfdc70d5d2a9
Tao BERQUER <tao.berquer@gmail.com>
2018-10-29 00:13:42 +0100

    Ajout de l'update des flux RSS.

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ require (dirname(__DIR__) . '/config.php');
 
 try
 {
-    $bdd = new PDO ("mysql:host=$host;dbname=$dbname",$user,$password);
+    $dbh = new PDO ("mysql:host=$host;dbname=$dbname",$user,$password);
 }
 catch(Exception $e)
 {
--- public/update.php
+++ public/update.php
@@ -1,1 +1,28 @@
 <?php
+
+$RssSources = $dbh->query('SELECT * FROM RSSSources')->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($RssSources as $RssSource) {
+    $rss = Feed::loadRss($RssSource['website']);
+
+    foreach ($rss->item as $item) {
+        $checkExistArticle = $dbh->prepare('SELECT * FROM Articles WHERE link = :link LIMIT 1');
+        $checkExistArticle->bindValue(':link', $item->link, PDO::PARAM_STR);
+        $checkExistArticle->execute();
+
+        if (!$checkExistArticle->fetch()) {
+            $addArticle = $dbh->prepare('
+                INSERT INTO `Articles` (`title`, `preview`, `date`, `link`, `rssSourceId`) 
+                VALUES (:title, :preview, :date, :link, :rssSourceId)
+            ');
+
+            $addArticle->bindValue(':title', $item->title, PDO::PARAM_STR);
+            $addArticle->bindValue(':preview', $item->description, PDO::PARAM_STR);
+            $addArticle->bindValue(':date', date('Y-m-d H:i:s', (int)$item->timestamp), PDO::PARAM_STR);
+            $addArticle->bindValue(':link', $item->link, PDO::PARAM_STR);
+            $addArticle->bindValue(':rssSourceId', $RssSource['id'], PDO::PARAM_INT);
+
+            $addArticle->execute();
+        }
+    }
+}
