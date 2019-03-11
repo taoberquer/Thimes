383eb6920cdc72ce4226ed7ab79ffd65655f22e8
Tao BERQUER <tao.berquer@gmail.com>
2019-03-11 10:19:00 +0100

    Ajout de twig.

--- src/dependencies.php
+++ src/dependencies.php
@@ -3,10 +3,18 @@
 
 $container = $app->getContainer();
 
-// view renderer
-$container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(dirname(__DIR__) . '/templates', [
+        'cache' => false, //'path/to/cache'
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
 };
 
 // monolog
