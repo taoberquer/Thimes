d0f6f37c867bc1b98375717992781347ad63a6a7
Tao BERQUER <tao.berquer@gmail.com>
2019-03-11 10:16:00 +0100

    Ajout de Eloquent à Slim.

--- src/dependencies.php
+++ src/dependencies.php
@@ -17,3 +17,21 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+// Service factory for the ORM
+$container['db'] = function ($container) {
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule->addConnection($container['settings']['db']);
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
+};
+
+$container[App\WidgetController::class] = function ($c) {
+    $view = $c->get('view');
+    $logger = $c->get('logger');
+    $table = $c->get('db')->table('table_name');
+    return new \App\WidgetController($view, $logger, $table);
+};
