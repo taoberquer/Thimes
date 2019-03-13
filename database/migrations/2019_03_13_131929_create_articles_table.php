bca9973a9fe6107da83c3627d852f39e2237f2f4
Tao BERQUER <tao.berquer@gmail.com>
2019-03-13 14:24:39 +0100

    Ajout de la table `articles` aux migrations sans les clés étrangères.

---
+++ database/migrations/2019_03_13_131929_create_articles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('flux_id');
+            $table->char('title');
+            $table->text('description');
+            $table->char('guid');
+            $table->text('url');
+            $table->char('source');
+            $table->dateTime('pub_date');
+            $table->integer('flux_batch');
+            $table->char('hash');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
