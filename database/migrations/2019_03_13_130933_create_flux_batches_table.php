82002ecf28d20d34a069da71f44d6178e4b6f731
Tao BERQUER <tao.berquer@gmail.com>
2019-03-13 15:34:59 +0100

    Ajout de la clé étrangère de la table `flux_batches` aux migrations.

--- database/migrations/2019_03_13_130933_create_flux_batches_table.php
+++ database/migrations/2019_03_13_130933_create_flux_batches_table.php
@@ -15,7 +15,7 @@ class CreateFluxBatchesTable extends Migration
     {
         Schema::create('flux_batches', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedInteger('flux_id');
+            $table->unsignedBigInteger('flux_id');
             $table->dateTime('started_at');
             $table->dateTime('ended_at');
             $table->boolean('success');
---
+++ database/migrations/2019_03_13_141310_update_flux_batches_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateFluxBatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('flux_batches', function (Blueprint $table) {
+            $table->foreign('flux_id')->references('id')->on('fluxes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
