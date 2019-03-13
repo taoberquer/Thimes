29d4846ad2a1c1b64308dac8ab57067860a38ffe
Tao BERQUER <tao.berquer@gmail.com>
2019-03-13 14:12:15 +0100

    Ajout de la table `flux_batches` aux migrations.

---
+++ database/migrations/2019_03_13_130933_create_flux_batches_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFluxBatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flux_batches', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('flux_id');
+            $table->dateTime('started_at');
+            $table->dateTime('ended_at');
+            $table->boolean('success');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flux_batches');
+    }
+}
