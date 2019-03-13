1f595959d00b98788b8f71d32adfe984f6b15e51
Tao BERQUER <tao.berquer@gmail.com>
2019-03-13 15:07:13 +0100

    Ajout de la clé étrangère de la table `users` aux migrations.

---
+++ database/migrations/2019_03_13_140456_update_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('club_id')->references('id')->on('clubs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropForeign('users_club_id_foreign');
+        });
+    }
+}
