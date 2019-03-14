3c87da6d6b2daa1ea12c982879b0acc8287dc6f1
Tao BERQUER <tao.berquer@gmail.com>
2019-03-14 10:36:11 +0100

    Possibilité de ne pas remplir le `club_id` dans la table `users`.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('status', ['user', 'club', 'admin'])->default('user');
-            $table->unsignedBigInteger('club_id');
+            $table->unsignedBigInteger('club_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
