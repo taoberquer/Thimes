9162a2d26fb541dca70b8635593b45a72b294911
Alex Kaprielian <alex.kaprielian@gmail.com>
2019-03-18 12:15:04 +0100

    Modification du Models SportArticle, il ne reste en getter que l'id.

--- app/SportArticle.php
+++ app/SportArticle.php
@@ -7,32 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class SportArticle extends Model
 {
     protected $hidden = [
-         'id', 'sport_id', 'club_id','article_id',
-         'user_id'
+         'id'
+
       ];
 
-    public function getId()
+    public function getId(): int
     {
         return $this->id;
     }
-
-    public function getSportId()
-    {
-        return $this->sport_id;
-    }
-
-    public function getClubId()
-    {
-        return $this->club_id;
-    }
-
-    public function getArticleId()
-    {
-        return $this->article_id;
-    }
-
-    public function getUserId()
-    {
-        return $this->user_id;
-    }
-}
