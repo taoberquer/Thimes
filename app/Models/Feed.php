acbba94b230f9ee5d91264e479cae1e4d50d5658
Tao BERQUER <tao.berquer@gmail.com>
2019-03-18 11:14:50 +0100

    Ajout de l'objet Feed créé par Mr Truffaut.

---
+++ app/Models/Feed.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+class Feed
+{
+    public function get($url)
+    {
+        return $this->parse($this->load($url));
+    }
+
+    public function load($url)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, "curl/7.54.0");
+        if (! $rawContent = curl_exec($curl)) {
+            trigger_error(curl_error($curl));
+        }
+        curl_close($curl);
+        return $rawContent;
+    }
+
+    public function parse($content)
+    {
+        $res = simplexml_load_string($content) or die("Error: Cannot create object");
+        return $res;
+    }
+}
