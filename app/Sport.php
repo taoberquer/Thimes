a75a0ef558512aaf9ac81e27d8e31ed1a257668c
Alex Kaprielian <alex.kaprielian@gmail.com>
2019-03-18 11:17:59 +0100

    Création des Getters pour le Model Sport.

--- app/Sport.php
+++ app/Sport.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sport extends Model
 {
-    //
+    protected $hidden = [
+         'id', 'sport_id', 'club_id','article_id',
+         'user_id'
+      ];
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getSportId()
+    {
+        return $this->sport_id;
+    }
+
+    public function getClubId()
+    {
+        return $this->club_id;
+    }
+
+    public function getArticleId()
+    {
+        return $this->article_id;
+    }
+
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
 }
