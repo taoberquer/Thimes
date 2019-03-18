f9ce1d33ca1e73d1afbf266159fc500c2f13ac78
Alex Kaprielian <alex.kaprielian@gmail.com>
2019-03-18 10:02:01 +0100

    Création des Getters pour le Model Club.

--- app/Club.php
+++ app/Club.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Club extends Model
 {
-    //
+    protected $hidden = [
+       'id', 'name'
+   ];
+
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
 }
