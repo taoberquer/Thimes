8d53b64cdcf44d1721cafaf417420f45ba3c8531
Alex Kaprielian <alex.kaprielian@gmail.com>
2019-03-18 11:11:56 +0100

    Création des Getters pour le Model FluxSport.

--- app/FluxSport.php
+++ app/FluxSport.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class FluxSport extends Model
 {
-    //
+    protected $hidden = [
+         'flux-id', 'sport_id'
+      ];
+
+    public function getFluxId()
+    {
+        return $this->flux_id;
+    }
+
+    public function getSportId()
+    {
+        return $this->sport_id;
+    }
 }
