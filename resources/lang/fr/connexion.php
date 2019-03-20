83ec0ebbe5dd329cc949ebe5de08c709627f4d03
Alex Kaprielian <alex.kaprielian@gmail.com>
2019-03-20 17:08:57 +0100

    Création du fichier de traduction de l'inscription.

---
+++ resources/lang/fr/connexion.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'login' => 'Connexion',
+    'login1' => 'Se connecter',
+    'E-Mail Address' => 'Adresse E-Mail',
+    'password' => 'Mot de passe',
+    'Remember Me' => 'Se souvenir de moi',
+    'Forgot Your Password?' => 'Mot de passe oublié ?'
+];
