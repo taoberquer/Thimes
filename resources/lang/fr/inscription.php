3e9e1223c510fad54fddee1e5b86f63e54a9d218
Alex Kaprielian <alex.kaprielian@gmail.com>
2019-03-20 17:19:19 +0100

    Création du fichier de traduction de l'inscription.

---
+++ resources/lang/fr/inscription.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'Register' => 'Inscription',
+    'Name' => 'Nom',
+    'E-Mail Address' => 'Adresse E-Mail',
+    'Password' => 'Mot de passe',
+    'Confirm Password' => 'Confirmez le mot de passe',
+    'Register1' => 'S\'inscrire'
+
+];
