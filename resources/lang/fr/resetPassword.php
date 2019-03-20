58fa4583431a0d66480995c06702c029497a471a
Alex Kaprielian <alex.kaprielian@gmail.com>
2019-03-20 17:30:54 +0100

    Création du fichier de traduction du reset password.

---
+++ resources/lang/fr/resetPassword.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'Reset Password' => 'Réinitialisation du mot de passe',
+    'E-Mail Address' => 'Adresse E-Mail',
+    'Send Password Reset Link' => 'Envoyer le lien de réinitialisation du mot de passe'
+];
