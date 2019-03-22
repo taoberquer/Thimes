1ee5e0a157e7023afbc456ebbe8aaaf2f4386b90
Alex Kaprielian <alex.kaprielian@gmail.com>
2019-03-22 04:09:40 +0100

    Première reflexion sur les card aside pour l'affichage des articles.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,7 +60,125 @@
                           <a href="#">Start Bootstrap</a>
                           on July 8, 2019</p>
                       </div>
+                      <hr />
+                      <div class="post-preview">
+                        <a href="post.html">
+                          <h2 class="post-title">
+                            Failure is not an option
+                          </h2>
+                          <h3 class="post-subtitle">
+                            Many say exploration is part of our destiny, but it’s actually our duty to future generations.
+                          </h3>
+                        </a>
+                        <p class="post-meta">Posted by
+                          <a href="#">Start Bootstrap</a>
+                          on July 8, 2019</p>
+                      </div>
                   </div>
+
+
+
+     <div class="col-md-4">
+
+         <!-- a voir si on garde la barre de recherche dans un aside ou on la place dans la navbar -->
+       <div class="card my-4">
+         <h5 class="card-header">Rechercher</h5>
+         <div class="card-body">
+           <div class="input-group">
+             <input type="text" class="form-control" placeholder="Recherche pour...">
+             <span class="input-group-btn">
+               <button class="btn btn-secondary" type="button">Go!</button>
+             </span>
+           </div>
+         </div>
+       </div>
+
+
+
+              <div class="card my-4">
+                <h5 class="card-header">Sources</h5>
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <ul class="list-unstyled mb-0">
+                        <li>
+                            <input type="checkbox" id="" value="" />
+                            <a href="#">L'Est Républicain</a>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="" value="" />
+                            <a href="#">Le Républicain Lorrain</a>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="" value="" />
+                            <a href="#">Vosges Matin</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="col-lg-6">
+                      <ul class="list-unstyled mb-0">
+                        <li>
+                           <input type="checkbox" id="" value="" />
+                           <a href="#">Les Dernières Nouvelles d’Alsace</a>
+                        </li>
+                        <li>
+                            <input type="checkbox" id="" value="" />
+                            <a href="#">L’Alsace Le Pays</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+
+       <div class="card my-4">
+         <h5 class="card-header">Catégories</h5>
+         <div class="card-body">
+           <div class="row">
+             <div class="col-lg-6">
+               <ul class="list-unstyled mb-0">
+                 <li>
+                     <input type="checkbox" id="" value="" />
+                     <a href="#">Football</a>
+                 </li>
+                 <li>
+                     <input type="checkbox" id="" value="" />
+                     <a href="#">Basketball</a>
+                 </li>
+                 <li>
+                     <input type="checkbox" id="" value="" />
+                     <a href="#">Tennis</a>
+                 </li>
+               </ul>
+             </div>
+             <div class="col-lg-6">
+               <ul class="list-unstyled mb-0">
+                 <li>
+                    <input type="checkbox" id="" value="" />
+                    <a href="#">Curling sur gazon</a>
+                 </li>
+                 <li>
+                     <input type="checkbox" id="" value="" />
+                     <a href="#">Badminton</a>
+                 </li>
+                 <li>
+                     <input type="checkbox" id="" value="" />
+                     <a href="#">Ski</a>
+                 </li>
+               </ul>
+             </div>
+           </div>
+         </div>
+       </div>
+
+
+   </div>
+   <!-- /.row -->
+
+ </div>
+ <!-- /.container -->
 
 
 
